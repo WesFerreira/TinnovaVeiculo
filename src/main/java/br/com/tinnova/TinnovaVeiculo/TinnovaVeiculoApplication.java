8dfe764a4d377b23186120db22e19c19a3fd0b1d
Wesley Ferreira da Silva <ferreiras.wesley@gmail.com>
2025-09-24 13:34:43 -0300

    first commit

---
+++ src/main/java/br/com/tinnova/TinnovaVeiculo/TinnovaVeiculoApplication.java
@@ -0,0 +1,13 @@
+package br.com.tinnova.TinnovaVeiculo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinnovaVeiculoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinnovaVeiculoApplication.class, args);
+	}
+
+}
